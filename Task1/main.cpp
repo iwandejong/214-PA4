005e924889323e13122ca0afe2b7a11beba62988
SW1F7YY <stephansmit112@gmail.com>
2023-10-10 09:39:36 +0200

    final hopefully

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,5 +1,40 @@
+//! checks token/nonce validity by checking if the value is 0.
+//! if yes then no token/nonce has been asked/received
+//! else it is correct
+#include "ActualRequest.h"
+#include "CheckToken.h"
+#include "handler.h"
+#include "RequestNonce.h"
+#include "user.h"
+#include "ValidateSignIn.h"
 #include <iostream>
 
 int main() {
+    //each main handler object has its own user.
+    user newUser;
+    handler* mainHandler = new handler();
+    mainHandler->add(new RequestNonce());
+    mainHandler->add(new ValidateSignIn());
+    mainHandler->add(new CheckToken());
+    mainHandler->add(new ActualRequest());
+    //! main commands: "Validate" = validate sign in,"Request" = request nonce,"Check" = check token, !="" = all other requests
+    
+    cout<<"Requesting Nonce:"<<endl;
+    mainHandler->handle("Request",newUser);
+    // cout<<endl;
+    cout<<endl;
+    cout<<"Validating"<<endl;
+    mainHandler->handle("Validate",newUser);
+    cout<<endl;
+    
+    // cout<<endl;
+    cout<<"Check Token"<<endl;
+    mainHandler->handle("Check",newUser);
+    cout<<endl;
+    // mainHandler->handle("Give me money",newUser);
+    // changed every handle to void from string
+    delete mainHandler;
+    
+
     return 0;
 }
--- Task1/server.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_H
-#define SERVER_H
-#include "handler.h"
-using namespace std;
-class server: public handler{
-
-};
-#endif
